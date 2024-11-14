5ca098a7590bbade005baeb326190203ac1090a6
drumboy02 <153551032+drumboy02@users.noreply.github.com>
2024-11-13 21:23:46 -0500

    modified createGrid function to take parameters for different size grid

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const gridSquareContainer = document.querySelector(".grid-square-div");
 
-function createGrid() {
-    for (let i = 0; i < 16; i++) {
+function createGrid(numberRows, numberColumns) {
+    for (let i = 0; i < numberRows; i++) {
         const gridRow = document.createElement("div");
         gridRow.classList = "grid-row";
         gridRow.id = `row-${i}`
 
-        for (let j = 0; j < 16; j++) {    
+        for (let j = 0; j < numberColumns; j++) {    
             const gridSquare = document.createElement("div");
             gridSquare.classList = "grid-square";
-            gridSquare.id = `square-${j}`
+            gridSquare.id = `row-${i}-square-${j}`
             gridSquare.style.visibility = "hidden";
             gridRow.appendChild(gridSquare);
         }
@@ -18,7 +18,13 @@ function createGrid() {
     }
 }
 
-createGrid()
+function sixteenBySixteen() {
+    createGrid(16, 16)
 
-const rows = document.querySelectorAll(".grid-row");
-const squares = document.querySelectorAll(".grid-square");
+    const rows = document.querySelectorAll(".grid-row");
+    const squares = document.querySelectorAll(".grid-square");
+
+    squares.forEach(square => square.style.visibility = "visible");
+}
+
+sixteenBySixteen();
