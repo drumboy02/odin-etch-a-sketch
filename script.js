6b27f1f1f054663cf27729d08dd7e04e919cf64e
drumboy02 <153551032+drumboy02@users.noreply.github.com>
2024-11-15 17:26:44 -0500

    added code to get height/width of grid squares

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ const btnSquares = document.querySelector(".btn-squares");
 
 btnSquares.addEventListener("click", () => numberOfSquares());
 
-function createGrid(numberRows, numberColumns) {
+function createGrid(numberRows=16, numberColumns=16) {
+    // create grid of squares with (nRows, nColumns)
+
     for (let i = 0; i < numberRows; i++) {
         const gridRow = document.createElement("div");
         gridRow.classList = "grid-row";
@@ -21,31 +23,48 @@ function createGrid(numberRows, numberColumns) {
     }
 }
 
-function etch() {
+function etch(color="black") {
+    // set grid squares to change color when mouse hovers
+
     const squares = document.querySelectorAll(".grid-square");
     squares.forEach(square => square.style.visibility = "visible");
 
     squares.forEach((square) => {
         square.addEventListener('mouseover', () => {
-            square.style.background = "black";
+            square.style.background = color;
         });
     });
 }
 
 function numberOfSquares() {
+    // prompt user for # of squares
     const number = Number(prompt("How many squares per side?"));
 
-    if (number < 1 || number > 99 || !number || number === NaN) {
-        alert("Choose a number between 1 and 99");
-        console.log("typeof: " + typeof(number));
+    if (number < 1 || number > 100 || !number || number === NaN) {
+        alert("Choose a number between 1 and 100");
         return;
     }
-    console.log("number: " + number);
-    return number;
+
+    // get current height/width of each grid square
+    const gridRows = document.querySelectorAll(".grid-row");
+    const gridSquares = document.querySelectorAll(".grid-square");
+
+    gridSquares.forEach(square => {
+        const sWidth = square.clientWidth;
+        const sHeight = square.clientHeight;
+
+        console.log("square width16: " + (sWidth * 16) / number);
+        console.log("square height16: " + (sHeight * 16) / number);
+        // clear the grid and call createGrid with new square sizes
+
+    });
+    
+    return;
 }
 
 function sixteenBySixteen() {
-    createGrid(16, 16);
+    // create grid with default values (16x16)   
+    createGrid();
     etch();
 }
 
